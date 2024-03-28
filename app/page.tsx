26c9f247abc433fee803af877dd2d874de9947ed
Otubusin Ademuyiwa <tay4real@gmail.com>
2024-03-28 06:54:34 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Farm Link Connect
-        </p>
-       
-      </div>
-
+    
       
-
-      
-    </main>
+  </main>
   );
 }
