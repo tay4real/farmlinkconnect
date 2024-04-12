85259b2c5625b5210dba2230df8c45e0813e0138
Otubusin Ademuyiwa <tay4real@gmail.com>
2024-04-12 09:31:38 +0100

    Update

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Farm Link Connect",
+  description: "FarmLink Connect is an innovative digital solution designed to revolutionize the way merchants, farmers, and investors interact and collaborate within the agricultural ecosystem.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
